ba2b3ceb3043caf1fc23c0aa185d2a9acf8e40d9
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2023-01-22 08:43:21 +0100

    fix vercel deploy issue

--- components/AppSuccessStories.vue
+++ components/AppSuccessStories.vue
@@ -5,7 +5,7 @@
             <div class="flex flex-wrap -mx-4">
                 <div class="w-full md:w-1/3 px-4 mb-8">
                     <div class="bg-white p-8 rounded-lg shadow-lg">
-                        <img class="w-12 h-12 rounded-full mx-auto mb-4" src="user1.jpg" alt="user 1">
+                        <img class="w-12 h-12 rounded-full mx-auto mb-4" src="~/assets/img/user.png" alt="user 1">
                         <p class="text-gray-600 mb-4">"I had an amazing experience with the course. The instructor was
                             knowledgeable and helpful. I would highly recommend this course to others."</p>
                         <h3 class="text-lg font-medium text-gray-700">User 1</h3>
@@ -13,7 +13,7 @@
                 </div>
                 <div class="w-full md:w-1/3 px-4 mb-8">
                     <div class="bg-white p-8 rounded-lg shadow-lg">
-                        <img class="w-12 h-12 rounded-full mx-auto mb-4" src="user2.jpg" alt="user 2">
+                        <img class="w-12 h-12 rounded-full mx-auto mb-4" src="~/assets/img/user.png" alt="user 2">
                         <p class="text-gray-600 mb-4">"I was impressed with the level of support and resources provided
                             by the team. The course exceeded my expectations and I would recommend it to anyone."</p>
                         <h3 class="text-lg font-medium text-gray-700">User 2</h3>
@@ -21,7 +21,7 @@
                 </div>
                 <div class="w-full md:w-1/3 px-4 mb-8">
                     <div class="bg-white p-8 rounded-lg shadow-lg">
-                        <img class="w-12 h-12 rounded-full mx-auto mb-4" src="user3.jpg" alt="user 3">
+                        <img class="w-12 h-12 rounded-full mx-auto mb-4" src="~/assets/img/user.png" alt="user 3">
                         <p class="text-gray-600 mb-4">"The course was well-organized and the material was easy to
                             understand. I've already applied what I learned in my work and I've seen positive results."
                         </p>
