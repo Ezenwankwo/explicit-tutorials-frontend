474402cd8fac6d593083650f8dd801121a6fbe4d
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2023-01-22 08:33:31 +0100

    deploy to vercel

--- components/AppFeaturedCourses.vue
+++ components/AppFeaturedCourses.vue
@@ -2,35 +2,35 @@
     <div class="bg-blue-200 py-12">
         <div class="container mx-auto px-4">
             <h2 class="text-3xl font-medium text-center text-white mb-8">Featured Courses</h2>
-            <div class="flex flex-wrap -mx-4">
+            <div class="flex flex-wrap">
                 <div class="w-full md:w-1/3 px-4 mb-8">
-                    <div class="bg-white p-8 rounded-lg shadow-lg">
-                        <img class="w-full h-48 object-cover mb-4" src="course1.jpg" alt="course 1">
-                        <h3 class="text-lg font-medium text-gray-700 mb-2">Course 1</h3>
-                        <p class="text-gray-600">Learn the basics of course 1</p>
-                        <div class="mt-4">
+                    <div class="bg-white rounded-lg shadow-lg">
+                        <img class="mb-4" src="~/assets/img/course.png" alt="course 1">
+                        <h3 class="text-lg font-medium text-gray-700 mb-2 p-6">Course 1</h3>
+                        <p class="text-gray-600 p-6">Learn the basics of course 1</p>
+                        <div class="mt-4 p-6">
                             <a href="#" class="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600">Enroll
                                 now</a>
                         </div>
                     </div>
                 </div>
                 <div class="w-full md:w-1/3 px-4 mb-8">
-                    <div class="bg-white p-8 rounded-lg shadow-lg">
-                        <img class="w-full h-48 object-cover mb-4" src="course2.jpg" alt="course 2">
-                        <h3 class="text-lg font-medium text-gray-700 mb-2">Course 2</h3>
-                        <p class="text-gray-600">Learn the basics of course 2</p>
-                        <div class="mt-4">
+                    <div class="bg-white rounded-lg shadow-lg">
+                        <img class="mb-4" src="~/assets/img/course.png" alt="course 1">
+                        <h3 class="text-lg font-medium text-gray-700 mb-2 p-6">Course 1</h3>
+                        <p class="text-gray-600 p-6">Learn the basics of course 1</p>
+                        <div class="mt-4 p-6">
                             <a href="#" class="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600">Enroll
                                 now</a>
                         </div>
                     </div>
                 </div>
                 <div class="w-full md:w-1/3 px-4 mb-8">
-                    <div class="bg-white p-8 rounded-lg shadow-lg">
-                        <img class="w-full h-48 object-cover mb-4" src="course3.jpg" alt="course 3">
-                        <h3 class="text-lg font-medium text-gray-700 mb-2">Course 3</h3>
-                        <p class="text-gray-600">Learn the basics of course 3</p>
-                        <div class="mt-4">
+                    <div class="bg-white rounded-lg shadow-lg">
+                        <img class="mb-4" src="~/assets/img/course.png" alt="course 1">
+                        <h3 class="text-lg font-medium text-gray-700 mb-2 p-6">Course 1</h3>
+                        <p class="text-gray-600 p-6">Learn the basics of course 1</p>
+                        <div class="mt-4 p-6">
                             <a href="#" class="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600">Enroll
                                 now</a>
                         </div>
