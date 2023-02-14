fbf23c3e95101154dff497fe2a7e92ef98eb83e9
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2023-02-15 00:02:41 +0100

    updated pricing UI

--- pages/classes.vue
+++ pages/classes.vue
@@ -1,15 +1,12 @@
 <template>
-    <div class="py-12 bg-blue-50">
+    <div class="py-12">
         <div class="container mx-auto px-8">
-            <h2 class="text-4xl font-bold text-center text-gray-700 mb-8">Featured classes</h2>
+            <h2 class="text-4xl font-bold text-center text-gray-700 mb-8">Our Classes</h2>
             <div class="flex flex-wrap">
                 <AppTutorialClass />
                 <AppTutorialClass />
                 <AppTutorialClass />
-            </div>
-            <div class="text-center text-blue-700">
-                <a class="link link-hover">View 8+ more classes</a>
-                <Icon name="bi:caret-right-fill" />
+                <AppTutorialClass />
             </div>
         </div>
     </div>
--- pages/pricing.vue
+++ pages/pricing.vue
@@ -1,9 +1,162 @@
 <template>
-    <div>hmm</div>
+    <div class="py-12">
+        <div class="container mx-auto px-8">
+            <h2 class="text-4xl font-bold text-center text-gray-700">
+                Students and Affiliate Plans and Pricing
+            </h2>
+            <p class="text-center text-gray-700 mb-8">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Debitis iste perspiciatis<br> commodi consequatur praesentium accusantium.
+            </p>
+            <div class="flex flex-wrap">
+                <div class="w-full md:w-1/4 px-4 mb-8">
+                    <div class="py-8">
+                        <div class="bg-[#ECF3FF] rounded-lg shadow-lg py-6">
+                            <img src="~/assets/img/student.png" class="mx-auto pb-6" alt="student image">
+                            <p class="text-lg text-center font-bold text-gray-700 mb-2">Student<br> Subscription Plans
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full md:w-1/3 px-4 mb-8">
+                    <div class="py-8">
+                        <div class="flex flex-col bg-[#CBDFFF] rounded-lg shadow-lg p-6">
+                            <h3 class="text-lg font-medium text-gray-700">Basic</h3>
+                            <p class="text-gray-600 py-4">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Debitis iste perspiciatis commodi consequatur praesentium accusantium.
+                            </p>
+                            <p class="text-lg font-bold text-gray-700 mb-4">&#8358; 5,000/month</p>
+                            <button
+                                class="btn capitalize bg-[#136FFB] border-[#136FFB] hover:bg-blue-700 hover:border-blue-700 focus:bg-blue-700 focus:border-blue-700">Select</button>
+                        </div>
+                        <ul role="list" class="pl-5 space-y-3 text-gray-600 mt-6 border-l border-[#136FFB]">
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#136FFB] mr-2" /> cups chopped Porcini
+                                mushrooms
+                            </li>
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#136FFB] mr-2" /> 1/2 cup of olive oil
+                            </li>
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#136FFB] mr-2" /> 3lb of celery
+                            </li>
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#136FFB] mr-2" /> cups chopped Porcini
+                                mushrooms
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="w-full md:w-1/3 px-4 mb-8">
+                    <div class="py-8">
+                        <div class="flex flex-col bg-[#FFD4B0] rounded-lg shadow-lg p-6">
+                            <h3 class="text-lg font-medium text-gray-700">Premium</h3>
+                            <p class="text-gray-600 py-4">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Debitis iste perspiciatis commodi consequatur praesentium accusantium.
+                            </p>
+                            <p class="text-lg font-bold text-gray-700 mb-4">&#8358; 10,000/month</p>
+                            <button
+                                class="btn capitalize bg-[#FF6600] border-[#FF6600] hover:bg-orange-700 hover:border-orange-700 focus:bg-orange-700 focus:border-orange-700">Select</button>
+                        </div>
+                        <ul role="list" class="pl-5 space-y-3 text-gray-600 mt-6 border-l border-[#FB9139]">
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#FB9139] mr-2" /> cups chopped Porcini
+                                mushrooms
+                            </li>
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#FB9139] mr-2" /> 1/2 cup of olive oil
+                            </li>
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#FB9139] mr-2" /> 3lb of celery
+                            </li>
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#FB9139] mr-2" /> cups chopped Porcini
+                                mushrooms
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <hr class="border-t border-[#136FFB]">
+            <div class="flex flex-wrap">
+                <div class="w-full md:w-1/4 px-4 mb-8">
+                    <div class="py-8">
+                        <div class="bg-[#B0FFC5] rounded-lg shadow-lg py-6">
+                            <img src="~/assets/img/affiliate.png" class="mx-auto pb-6" alt="student image">
+                            <p class="text-lg text-center font-bold text-gray-700 mb-2">Affiliate<br> Subscription Plans
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full md:w-1/3 px-4 mb-8">
+                    <div class="py-8">
+                        <div class="flex flex-col bg-[#CBDFFF] rounded-lg shadow-lg p-6">
+                            <h3 class="text-lg font-medium text-gray-700">Basic</h3>
+                            <p class="text-gray-600 py-4">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Debitis iste perspiciatis commodi consequatur praesentium accusantium.
+                            </p>
+                            <p class="text-lg font-bold text-gray-700 mb-4">&#8358; 5,000/month</p>
+                            <button
+                                class="btn capitalize bg-[#136FFB] border-[#136FFB] hover:bg-blue-700 hover:border-blue-700 focus:bg-blue-700 focus:border-blue-700">Select</button>
+                        </div>
+                        <ul role="list" class="pl-5 space-y-3 text-gray-600 mt-6 border-l border-[#136FFB]">
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#136FFB] mr-2" /> cups chopped Porcini
+                                mushrooms
+                            </li>
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#136FFB] mr-2" /> 1/2 cup of olive oil
+                            </li>
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#136FFB] mr-2" /> 3lb of celery
+                            </li>
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#136FFB] mr-2" /> cups chopped Porcini
+                                mushrooms
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="w-full md:w-1/3 px-4 mb-8">
+                    <div class="py-8">
+                        <div class="flex flex-col bg-[#FFD4B0] rounded-lg shadow-lg p-6">
+                            <h3 class="text-lg font-medium text-gray-700">Premium</h3>
+                            <p class="text-gray-600 py-4">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Debitis iste perspiciatis commodi consequatur praesentium accusantium.
+                            </p>
+                            <p class="text-lg font-bold text-gray-700 mb-4">&#8358; 10,000/month</p>
+                            <button
+                                class="btn capitalize bg-[#FF6600] border-[#FF6600] hover:bg-orange-700 hover:border-orange-700 focus:bg-orange-700 focus:border-orange-700">Select</button>
+                        </div>
+                        <ul role="list" class="pl-5 space-y-3 text-gray-600 mt-6 border-l border-[#FB9139]">
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#FB9139] mr-2" /> cups chopped Porcini
+                                mushrooms
+                            </li>
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#FB9139] mr-2" /> 1/2 cup of olive oil
+                            </li>
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#FB9139] mr-2" /> 3lb of celery
+                            </li>
+                            <li>
+                                <Icon name="bi:patch-check-fill" class="text-[#FB9139] mr-2" /> cups chopped Porcini
+                                mushrooms
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 definePageMeta({
-  layout: "external",
+    layout: "external",
 });
 </script>
